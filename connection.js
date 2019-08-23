b8c73487b4b3ff5a00aae3bcb28b38a90a87e3d5
malvarius <e.luismalvar@gmail.com>
2019-08-22 18:03:56 -0700

    adding remote server

--- connection.js
+++ connection.js
@@ -1,12 +1,15 @@
 var mysql = require("mysql")
 require('dotenv').config();
-
-var connection = mysql.createConnection({
+var connection ;
+if(process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else {connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: process.env.PW,
     database: "burgers_db"
   });
+}
 
   module.exports=connection;
